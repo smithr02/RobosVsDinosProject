daeb85d0303940d1b3c687daf870b364001795b8
Ryan Smith <ryan_smith2@hotmail.com>
2020-04-13 17:05:13 -0500

    Redid list on herd and fleet for better optimization

--- RobotsVersusDinosaurs/Fleet.cs
+++ RobotsVersusDinosaurs/Fleet.cs
@@ -8,21 +8,14 @@ namespace RobotsVersusDinosaurs
 {
     class Fleet
     {
-        public Robots HugeMech; 
-        public Robots MediumMech;
-        public Robots SmallMech;
-
-        public List<Robots> Robots = new List<Robots>(); //creating a new list and storing it. 
+        public List<Robots> myRobots;
 
         public Fleet()
         {
-            HugeMech = new Robots("HugeMech", 500, 200, 100);  //setting the variables that are in robot class with specific name and values.
-            MediumMech = new Robots("MediumMech", 300, 300, 50);
-            SmallMech = new Robots("SmallMech", 200, 400, 25);
-
-            Robots.Add(HugeMech);    //adding each individual robot object to the list, i.e. adding HugeMech etc.
-            Robots.Add(MediumMech);
-            Robots.Add(SmallMech);
+            myRobots = new List<Robots>();
+            myRobots.Add(new Robots("GiantMech", 500, 200, 100));
+            myRobots.Add(new Robots("MediumMech", 300, 300, 50));
+            myRobots.Add(new Robots("SmallMech", 200, 400, 25));
         }
     }
 }
--- RobotsVersusDinosaurs/Herd.cs
+++ RobotsVersusDinosaurs/Herd.cs
@@ -8,21 +8,15 @@ namespace RobotsVersusDinosaurs
 {
     class Herd
     {
-        public Dinosaurs LargeDino;
-        public Dinosaurs MediumDino;
-        public Dinosaurs SmallDino;
-
-        public List<Dinosaurs> Dinosaurs = new List<Dinosaurs>(); //created new list and stored it as Dinosaurs.
+        
+        public List<Dinosaurs> myDinosaurs; //created new list and stored it as Dinosaurs.
 
         public Herd()
         {
-            LargeDino = new Dinosaurs("LargeDino", 500, 200, 100); //adding name and stats to the parameters in Dinosaurs constructor.
-            MediumDino = new Dinosaurs("MediumDino", 300, 300, 50);
-            SmallDino = new Dinosaurs("SmallDino", 200, 400, 25);
-
-            Dinosaurs.Add(LargeDino); //adding the dinos to the list.
-            Dinosaurs.Add(MediumDino);
-            Dinosaurs.Add(SmallDino);
+            myDinosaurs = new List<Dinosaurs>();
+            myDinosaurs.Add(new Dinosaurs("LargeDino", 500, 200, 100));
+            myDinosaurs.Add(new Dinosaurs("MediumDino", 300, 300, 50));
+            myDinosaurs.Add(new Dinosaurs("SmallDino",200, 400, 25));
         }
     }
 }
