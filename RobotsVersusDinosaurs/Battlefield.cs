55715c2b0167737f9fd1e805f541217dfa665a4f
Ryan Smith <ryan_smith2@hotmail.com>
2020-04-13 23:19:02 -0500

    added attack sequence methods to battlefield, completed run game method, potentially working on re working isGameOver method as having trouble accessing the lists from fleet and herd.

--- RobotsVersusDinosaurs/Battlefield.cs
+++ RobotsVersusDinosaurs/Battlefield.cs
@@ -19,9 +19,68 @@ namespace RobotsVersusDinosaurs
 
         public void RunGame()
         {
-            while (!IsGameFinished())
+            while (!IsGameOver())
             {
                 AttackSequence();
+            }
+        }
+
+        public bool IsGameOver()
+        {
+            bool returnValue = false;
+
+            if ((dinoFighters.myDinosaurs[i].)
+            {
+                returnValue = true;
+            }
+            return returnValue;
+        }
+
+        public void AttackSequence()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (roboFighters.myRobots[i].dead == true)
+                {
+                    continue;
+                }
+
+                Console.WriteLine($"It's now {roboFighters.myRobots[i].name} turn to attack!");
+                Console.WriteLine("Who do you chose to attack? Pick 1 for (LargeDino), Pick 2 for (MediumDino), Pick 3 for (SmallDino)");
+                int userInput = int.Parse(Console.ReadLine());
+
+                if (dinoFighters.myDinosaurs[userInput-1].dead == true)
+                {
+                    Console.WriteLine("That choice is invalid, please choose again!!");
+                    i--;
+                }
+                else
+                {
+                    roboFighters.myRobots[i].Attack(dinoFighters.myDinosaurs[userInput - 1]);
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (dinoFighters.myDinosaurs[i].dead == true)
+                {
+                    continue;
+                }
+
+                Console.WriteLine($"It is {dinoFighters.myDinosaurs[i].name} turn to attack!" );
+                Console.WriteLine("Who do you choose to attack? Pick 1 for (GiantMech) Pick 2 for (MediumMech) Pick 3 for (SmallMech)");
+                int userInput = int.Parse(Console.ReadLine());
+
+                if (roboFighters.myRobots[userInput -1].dead == true)
+                {
+                    Console.WriteLine("That choice is invalid, please choose again!");
+                    i--;
+                }
+                else
+                {
+                    dinoFighters.myDinosaurs[i].Attack(roboFighters.myRobots[userInput - 1]);
+                }
+
             }
         }
     }
